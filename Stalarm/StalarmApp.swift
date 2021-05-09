f0a86e88b21e489f270b4323ced34ae08f1e044c
Andrean Lay <andreanlay1@gmail.com>
2021-05-09 11:33:17 +0700

    Initial Commit

---
+++ Stalarm/StalarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  StalarmApp.swift
+//  Stalarm
+//
+//  Created by Andrean Lay on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct StalarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
